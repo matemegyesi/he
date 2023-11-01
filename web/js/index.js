bd2073cb4f89fb89421735d51d059ba57cf59260
Megyesi Máté <megyesimate@gmail.com>
2023-11-01 13:03:36 +0100

    slide arrow

--- web/js/index.js
+++ web/js/index.js
@@ -13,3 +13,53 @@ function markdown(path, id) {
 }
 
 const markdowndiv = document.getElementById('../person_who_returned_from_the_dead.md', 'markdown');
+
+function putarrow(id, corner) {
+    const element = document.getElementById(id);
+    element.style.position = 'relative';
+
+    const arrow = document.createElement("div");
+    arrow.style.position = 'absolute';
+    arrow.classList.add('arrow');
+
+    switch (corner) {
+        case 'topright':
+            arrow.style.top = '0px';
+            arrow.style.right = '0px';
+            break;
+        case 'topleft':
+            arrow.style.top = '0px';
+            arrow.style.left = '0px';
+            break;
+        case 'bottomleft':
+            arrow.style.bottom = '0px';
+            arrow.style.left = '0px';
+            break;
+        case 'bottomright':
+            arrow.style.bottom = '0px';
+            arrow.style.right = '0px';
+            break;
+        default:
+            break;
+    }
+    
+
+    const image = document.createElement("img");
+    image.src = '/web/src/images/arrow.svg';
+    image.width = 100;
+    image.height = 100;
+    image.classList.add('filter-white');
+
+    arrow.appendChild(image);
+    element.appendChild(arrow);
+
+    element.addEventListener('mouseover', function(){
+        arrow.classList.add('arrow-hover');
+    });
+    element.addEventListener('mouseleave', function(){
+        arrow.classList.remove('arrow-hover');
+    });
+}
+
+putarrow('slide', 'bottomright');
+
